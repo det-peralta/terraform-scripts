d8074de9849fe705fabc531373b97aad8a46aaf5
det-peralta <det-peralta@outlook.com>
2024-05-27 16:42:30 +0100

    fix typo

--- create-vm-proxmox/provider.tf
+++ create-vm-proxmox/provider.tf
@@ -7,16 +7,6 @@ terraform {
   }
 }
 
-# provider "proxmox" {
-#   endpoint  = var.virtual_environment_endpoint
-#   api_token = var.virtual_environment_token
-#   insecure = true
-#   ssh {
-#     agent    = true
-#     username = "sleepy"
-#   }
-# }
-
 provider "proxmox" {
   endpoint = var.virtual_environment_endpoint
   username = var.virtual_environment_username
